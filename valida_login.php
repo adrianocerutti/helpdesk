72d3fd37fc16a1742fdd4e7248d7c86da0744f28
Adriano Cerutti <adrianocerutti@gmail.com>
2022-10-06 17:08:48 -0300

    Protegendo páginas restritas com SESSION

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 // variável que verifica se a autenticação foi realizada
 $usuario_autenticado = false;
 
@@ -21,6 +23,8 @@ foreach ($usuarios_app as $user) {
 
 if ($usuario_autenticado) {
     echo "Usuário autenticado";
+    $_SESSION['autenticado'] = 'SIM';
 } else {
+    $_SESSION['autenticado'] = 'NAO';
     header('Location: index.php?login=erro');
 }
